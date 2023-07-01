c5589b4848ea56eff929095217724fbfb9041406
cgates491 <135079985+cgates491@users.noreply.github.com>
2023-07-01 07:54:20 +0000

    question 2

--- test/question_test_2/question_tests_2.cpp
+++ test/question_test_2/question_tests_2.cpp
@@ -15,3 +15,13 @@ TEST_CASE("test binary to decimal")
 {
 	REQUIRE(binary_to_decimal("00001010") == 10);
 }
+
+TEST_CASE("test binary to decimal1")
+{
+	REQUIRE(binary_to_decimal("10101010") == 170);
+}
+
+TEST_CASE("test binary to decimal2")
+{
+	REQUIRE(binary_to_decimal("11111111") == 255);
+}
